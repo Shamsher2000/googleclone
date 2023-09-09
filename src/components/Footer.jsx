c6a21d751742e8a2dd6ae54daca0112105710c43
Shamsher2000 <shamshertiwari.nov2000@gmail.com>
2023-09-09 11:03:30 +0530

    pushing

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const Footer = () => (
+  <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200 ">
+    © 2022 Goggl, Inc.
+    MAde by SHAMSHER TIWARI
+  </div>
+);
---
+++ src/contexts/StateContextProvider.js
@@ -0,0 +1,36 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const StateContext = createContext();
+const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
+
+export const StateContextProvider = ({ children }) => {
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const getResults = async (url) => {
+    setLoading(true);
+    const res = await fetch(`${baseUrl}${url}`, {
+      method: 'GET',
+      headers: {
+        'X-User-Agent': 'desktop',
+        'X-Proxy-Location': 'IN',
+        'X-RapidAPI-Key': '591b5b40fcmshd5df5e2b503e786p15beacjsn094ac4ac1f4e',
+        'X-RapidAPI-Host': 'google-search3.p.rapidapi.com',
+      },
+    });
+
+    const data = await res.json();
+
+    setResults(data);
+    setLoading(false);
+  };
+
+  return (
+    <StateContext.Provider value={{ getResults, results, searchTerm, setSearchTerm, loading }}>
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
